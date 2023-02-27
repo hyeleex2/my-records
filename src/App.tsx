5044c12182bb36c5e5fb28bddcad2d9278d3cf03
OPENGA_Dev_hllee-at-900616242684 <hl.lee@quintet.co.kr>
2023-02-27 14:35:23 +0900

    initialized

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from "solid-js";
+
+import styles from "./App.module.css";
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
